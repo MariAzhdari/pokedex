7629fe886daea8b00825a3a4afc1229fbf27567e
Mari Azh <Marzi.aghchay@gmail.com>
2023-02-20 16:50:20 +0000

    react week 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from "./logo.svg";
-import "./App.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo />
+      <BestPokemon />
+      <CaughtPokemon/>
     </div>
   );
 }
+
+const Logo = () =>{
+  const appName = "Mari";
+  return (
+    <header>
+      <h1>Welcome to {appName}'s the Pokedex</h1>
+      <img
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+        alt="pokedex"
+      />
+    </header>
+  );
+};
+
+
+const BestPokemon = () => {
+  const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+  return (
+    <div>
+      <p> My favorite Pokemon is Squirtle</p>;
+      <ul>
+        {abilities.map((ability) =>(
+       <li>{ability}</li>
+      ))}
+      </ul>
+    </div>
+  );
+};
+
+const CaughtPokemon = () => {
+const date = new Date().toLocaleDateString();
+return <p>caught 0 pokemin on {date}</p>;
+};
+
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
 
 export default App;
