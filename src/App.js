704d6ea40cbc8bc831d07460deafec8c4c79cbef
Mari Azh <Marzi.aghchay@gmail.com>
2023-03-01 12:11:00 +0000

    Exercise A week 2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import BestPokemon from "./BestPokemon";
 import Logo from "./Logo";
 import CaughtPokemon from "./CaughtPokeman";
 
+const abilities =["Anticipation", "Adaptability", "Run-Away"];
 
 function App() {
   return (
     <div>
       <Logo appName ="Mari"/>
-      <BestPokemon  abilities= {["Anticipation", "Adaptability", "Run-Away"]}/>
+      <BestPokemon  abilities= {abilities}/>
       <CaughtPokemon />
     </div>
   );
--- src/Logo.js
+++ src/Logo.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Logo = (props) => {
+  const logWhenClicked =()=>{
+console.log("it doesn't matter what the message is");
+  };
   return (
     <header>
       <h1>Welcome to {props.appName}'s the Pokedex</h1>
-      <img
+      <img onClick={logWhenClicked}
         src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
         alt="pokedex"
       />
